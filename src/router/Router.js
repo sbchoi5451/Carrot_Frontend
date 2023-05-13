8360f8a5eddf4c3eb78031964354123a734aa1e4
jisoo <doh_0112@naver.com>
2023-05-13 15:42:07 +0900

    [chore] GlobalStyle에서 ` 추가, SignUp 컴포넌트 경로 오타 수정 : PJS

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MainPage from '../pages/MainPage'
-import DetailPage from '../pages/DetailPage'
-import SearchPage from '../pages/SearchPage'
-import LoginPage from '../pages/LoginPage'
-import SignupPage from '../pages/SignupPage'
-import MyPage from '../pages/MyPage'
-import WritingPage from '../pages/WritingPage'
-import styled from 'styled-components'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "../pages/MainPage";
+import DetailPage from "../pages/DetailPage";
+import SearchPage from "../pages/SearchPage";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import MyPage from "../pages/MyPage";
+import WritingPage from "../pages/WritingPage";
+import styled from "styled-components";
 
 function Router() {
   return (
@@ -18,19 +18,21 @@ function Router() {
           <Route path="/datil/:id" element={<DetailPage />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/writing" element={<WritingPage />} />
-          <Route path="/singup" element={<SignupPage />} />
+          <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/mypage" element={<MyPage />} />
         </Routes>
       </GlobalStyle>
     </BrowserRouter>
-  )
+  );
 }
 
-export default Router
+export default Router;
 
 const GlobalStyle = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 896px;
+  margin: 0 auto;
+`;
