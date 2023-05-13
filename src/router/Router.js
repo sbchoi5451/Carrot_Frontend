d778cff9057a1a6868ba8cb81e954533a7e26fcc
sbchoi5451 <sbchoi5451@gmail.com>
2023-05-13 09:13:47 +0900

    [STYLE] global style 추가

--- src/router/Router.js
+++ src/router/Router.js
@@ -7,21 +7,30 @@ import LoginPage from '../pages/LoginPage'
 import SignupPage from '../pages/SignupPage'
 import MyPage from '../pages/MyPage'
 import WritingPage from '../pages/WritingPage'
+import styled from 'styled-components'
 
 function Router() {
   return (
     <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<MainPage />}/>
-        <Route path="/datil/:id" element={<DetailPage />}/>
-        <Route path="/search" element={<SearchPage />}/>
-        <Route path="/writing" element={<WritingPage />}/>
-        <Route path="/singup" element={<SignupPage />}/>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/mypage" element={<MyPage />}/>
-    </Routes>
+      <GlobalStyle>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/datil/:id" element={<DetailPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/writing" element={<WritingPage />} />
+          <Route path="/singup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </GlobalStyle>
     </BrowserRouter>
   )
 }
 
 export default Router
+
+const GlobalStyle = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`
