fe9db41dca50becd3ec0882c30b2fc8c49b61627
jisoo <doh_0112@naver.com>
2023-05-15 20:56:54 +0900

    [chore] Provider 생성 PJS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
-import Router from './router/Router';
+import "./App.css";
+import Router from "./router/Router";
+import { Provider } from "react-redux";
+import store from "./redux/config/configStore";
 
 function App() {
-  return <Router />
+  return (
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  );
 }
 
 export default App;
