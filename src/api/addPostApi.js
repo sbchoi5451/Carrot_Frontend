08843801c6efa5a9c75e7b45e2bc13a16112a0b3
jisoo <doh_0112@naver.com>
2023-05-18 00:56:14 +0900

    이미지 파일 전송 코드 변경

--- src/api/addPostApi.js
+++ src/api/addPostApi.js
@@ -16,47 +16,47 @@ const getRefreshToken = () => {
 };
 
 // 판매글 작성 api
-// export const fetchAddPost = async (post, images) => {
-//   try {
-//     const formData = new FormData();
-
-//     // 게시글 내용 전부 post(객체) 변수에 저장
-//     for (let key in post) {
-//       formData.append(key, post[key]);
-//     }
-
-//     // 이미지 파일은 images[]라는 키값의 배열 형태로 저장
-//     images.forEach((image) => {
-//       formData.append("image[]", image);
-//     });
-
-//     const response = await axiosInstance.post("/post/add", formData, {
-//       headers: {
-//         "Content-Type": "multipart/form-data",
-//         access_key: getAccessToken(),
-//         refresh_key: getRefreshToken(),
-//       },
-//     });
-
-//     console.log("성공시 응답부분임", response);
-//     return response;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-export const fetchAddPost = async (post) => {
+export const fetchAddPost = async (post, images) => {
   try {
-    const response = await axiosInstance.post("/post/add", post, {
+    const formData = new FormData();
+
+    // 게시글 내용 전부 post(객체) 변수에 저장
+    for (let key in post) {
+      formData.append(key, post[key]);
+    }
+
+    // 이미지 파일은 images[]라는 키값의 배열 형태로 저장
+    images.forEach((image) => {
+      formData.append("image[]", image);
+    });
+
+    const response = await axiosInstance.post("/post/add", formData, {
       headers: {
+        "Content-Type": "multipart/form-data",
         access_key: getAccessToken(),
         refresh_key: getRefreshToken(),
       },
     });
+
     console.log("성공시 응답부분임", response);
-    return;
+    return response;
   } catch (err) {
-    console.log("내가보내는 데이터", post);
     console.log(err);
   }
 };
+
+// export const fetchAddPost = async (post) => {
+//   try {
+//     const response = await axiosInstance.post("/post/add", post, {
+//       headers: {
+//         access_key: getAccessToken(),
+//         refresh_key: getRefreshToken(),
+//       },
+//     });
+//     console.log("성공시 응답부분임", response);
+//     console.log("내가보내는 데이터", post);
+//     return;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
