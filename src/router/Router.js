ee0773fed5b03628724ff7b28586cda30c8c1dce
jisoo <doh_0112@naver.com>
2023-05-17 11:33:24 +0900

    회원가입 페이지 경로 변경 및 글로벌스타일 max-width 변경

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MainPage from '../pages/MainPage'
-import DetailPage from '../pages/DetailPage'
-import SearchPage from '../pages/SearchPage'
-import LoginPage from '../pages/LoginPage'
-import SignupPage from '../pages/SignupPage'
-import MyPage from '../pages/MyPage'
-import WritingPage from '../pages/WritingPage'
-import styled from 'styled-components'
-import ListPage from '../pages/ListPage'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "../pages/MainPage";
+import DetailPage from "../pages/DetailPage";
+import SearchPage from "../pages/SearchPage";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import MyPage from "../pages/MyPage";
+import WritingPage from "../pages/WritingPage";
+import styled from "styled-components";
+import ListPage from "../pages/ListPage";
 
 function Router() {
   return (
@@ -21,7 +21,7 @@ function Router() {
 
           <Route path="/post/add" element={<WritingPage />} />
           <Route path="/list" element={<ListPage />} />
-          <Route path="/singup" element={<SignupPage />} />
+          <Route path="/signup" element={<SignupPage />} />
 
           <Route path="/login" element={<LoginPage />} />
           <Route path="/mypage" element={<MyPage />} />
@@ -34,12 +34,11 @@ function Router() {
 export default Router;
 
 const GlobalStyle = styled.div`
-
-justify-content: center;
-align-items: center;
-display: flex;
-flex-direction: column;
-margin: 0 auto;
-height: 100%;
-max-width: 1440px;
-`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  height: 100%;
+  max-width: 1200px;
+`;
