604b32d98f90c24b605b3ccb54f83e45251768f7
jisoo <doh_0112@naver.com>
2023-05-18 18:23:06 +0900

    메인 슬라이더 사진 추가

--- src/components/main/MainSlide.jsx
+++ src/components/main/MainSlide.jsx
@@ -1,65 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import * as st from './slideST'
-import { useMutation, useQuery, useQueryClient } from 'react-query';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { getPost, getRecommendPost } from '../../api/postApi';
+import React, { useEffect, useState } from "react";
+import * as st from "./slideST";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { getPost, getRecommendPost } from "../../api/postApi";
 
 function MainSlide() {
-
   const navigation = useNavigate();
 
   const naviDetailBtn = ({ id }) => {
-    navigation(`/detail/${id}`)
-  }
+    navigation(`/detail/${id}`);
+  };
 
   const [currIndex, setCurrIndex] = useState(0);
 
   const queryClient = useQueryClient();
 
-  const { data: slideData = [] } = useQuery('mainSlide', getPost) //recommend 로 수정 필요
+  const { data: slideData = [] } = useQuery("mainSlide", getPost); //recommend 로 수정 필요
 
-  const sortingData = slideData?.sort((a,b) => b.interestCount - a.interestCount)?.slice(0,4)
+  const sortingData = slideData?.sort((a, b) => b.interestCount - a.interestCount)?.slice(0, 3);
+
+  // 사진 인덱스 번호
+  let number = 1;
 
   const prevHandler = () => {
-    setCurrIndex((index) => (index === 0 ? sortingData.length - 1 : index - 1))
-  }
+    setCurrIndex((index) => (index === 0 ? sortingData.length - 1 : index - 1));
+  };
 
   const nextHandler = () => {
-    setCurrIndex((index) => (index === sortingData.length - 1 ? 0 : index + 1))
-  }
+    setCurrIndex((index) => (index === sortingData.length - 1 ? 0 : index + 1));
+  };
 
   return (
     <>
       <st.SlideLayout>
         <st.SlideBody>
           <st.LeftMoveBtn onClick={prevHandler} />
-          <st.Wrapper width='70%'>
+          <st.Wrapper width="70%">
             {sortingData?.map((item, index) => (
-              <st.SlideContent key={index} active={index === currIndex ? 'true' : 'false'}>
-                <div className='img' justify='flex-end'>
-                  <st.ImgStyle src={item.postImage} onClick={() => naviDetailBtn({ id: item.id })} />
+              <st.SlideContent key={index} active={index === currIndex ? "true" : "false"}>
+                <div className="img" justify="flex-end">
+                  <st.ImgStyle src={`/img/tradeImg${number++}.jpeg`} onClick={() => naviDetailBtn({ id: item.id })} />
                 </div>
 
-                <div className='content' justify='flex-start'>
+                <div className="content" justify="flex-start">
                   <st.TextStyle>
                     <h3>{item.postTitle}</h3>
-                    <p className='price'>{`${parseInt(item.postPrice).toLocaleString()}원`}</p>
-                    <p className='tradelocation'>{item.tradeLocation}</p>
-                    <p className='interestcount'>{`관심 ${item.interestCount}`}</p>
+                    <p className="price">{`${parseInt(item.postPrice).toLocaleString()}원`}</p>
+                    <p className="tradelocation">{item.tradeLocation}</p>
+                    <p className="interestcount">{`관심 ${item.interestCount}`}</p>
                   </st.TextStyle>
                 </div>
-
               </st.SlideContent>
             ))}
-
           </st.Wrapper>
           <st.RightMoveBtn onClick={nextHandler} />
-
         </st.SlideBody>
       </st.SlideLayout>
     </>
-  )
+  );
 }
 
-export default MainSlide
+export default MainSlide;
--- src/components/main/slideST.js
+++ src/components/main/slideST.js
@@ -1,68 +1,74 @@
-import styled, {css} from 'styled-components'
+import styled, { css } from "styled-components";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 export const SlideLayout = styled.div`
-background-color: #fbf7f2;
-width: 100%;
-margin-top:150px;
-height: 550px;
-display: flex;
-justify-content: center;
-`
+  background-color: #fbf7f2;
+  width: 100%;
+  margin-top: 150px;
+  height: 550px;
+  display: flex;
+  justify-content: center;
+`;
 
 export const SlideBody = styled.div`
-width: 100%;
-height: 100%;
-display: flex;
-align-items: center;
-justify-content: space-around;
-`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
 
 export const Wrapper = styled.div`
-width: ${props => props.width};
-height: ${props => props.height};
-display: flex;
-align-items: center;
-justify-content: ${props => props.justify};
-`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) => props.justify};
+`;
 
 export const TextStyle = styled.div`
-display: flex;
-flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const ImgStyle = styled.img`
-width: 100%;
-height: 100%;
-object-fit: contain;
-border: transparent;
-border-radius: 10px;
-`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  border: transparent;
+  border-radius: 10px;
+  max-width: 400px;
+  max-height: 450px;
+`;
 
 export const LeftMoveBtn = styled(BsChevronLeft)`
-font-size: 50px;
-padding-left: 15px;
-&:hover {
+  font-size: 50px;
+  padding-left: 15px;
+  &:hover {
     cursor: pointer;
-}
-`
+  }
+`;
 
 export const RightMoveBtn = styled(BsChevronRight)`
-font-size: 50px;
-padding-left: 15px;
-&:hover {
+  font-size: 50px;
+  padding-left: 15px;
+  &:hover {
     cursor: pointer;
-}
-`
+  }
+`;
 
 export const SlideContent = styled.div`
-width: 100%;
-height: 100%;
-display: ${(props) => (props.active === 'true' ? 'flex' : 'none')};
-align-items: center;
-justify-content: center;
+  width: 100%;
+  height: 100%;
+  display: ${(props) => (props.active === "true" ? "flex" : "none")};
+  align-items: center;
+  justify-content: center;
 
-${(props) => props.active === 'true' && css` animation: slide-in 0.5s ease-in-out;`}
+  ${(props) =>
+    props.active === "true" &&
+    css`
+      animation: slide-in 0.5s ease-in-out;
+    `}
 
   @keyframes slide-in {
     0% {
@@ -75,13 +81,12 @@ ${(props) => props.active === 'true' && css` animation: slide-in 0.5s ease-in-ou
     }
   }
 
-.img {
+  .img {
     width: 70%;
     margin-left: 20px;
-}
+  }
 
-
-.content {
+  .content {
     width: 30%;
     margin-left: 20px;
 
@@ -98,5 +103,5 @@ ${(props) => props.active === 'true' && css` animation: slide-in 0.5s ease-in-ou
       font-size: 12px;
       color: gray;
     }
-}
-`
+  }
+`;
